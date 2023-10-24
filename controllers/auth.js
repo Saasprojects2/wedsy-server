791935b9691dc0254bf36c1eaa87de289fdd774c
hariomvc <chaudharihariomvijay@gmail.com>
2023-10-24 21:59:18 +0530

    Added Admin Login

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,8 @@ const User = require("../models/User");
 const { VerifyOTP, SendOTP } = require("../utils/otp");
 const jwt = require("jsonwebtoken");
 const jwtConfig = require("../config/jwt");
+const Admin = require("../models/Admin");
+const { CheckHash, CreateHash } = require("../utils/password");
 
 const Login = (req, res) => {
   const { name, phone, Otp, ReferenceId } = req.body;
@@ -60,10 +62,77 @@ const Login = (req, res) => {
   }
 };
 
+const AdminLogin = (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400).send({ message: "Incomplete Data" });
+  } else {
+    Admin.findOne({ email })
+      .then(async (user) => {
+        if (user) {
+          const { _id } = user;
+          if (
+            password &&
+            user.password &&
+            (await CheckHash(password, user.password))
+          ) {
+            const token = jwt.sign(
+              { _id, isAdmin: true },
+              process.env.JWT_SECRET,
+              jwtConfig
+            );
+            res.send({
+              message: "Login Successful",
+              token,
+            });
+          } else {
+            res.status(401).send({ message: "Wrong Credentials" });
+          }
+        } else if (false && req.body.phone && req.body.name && req.body.roles) {
+          const hashedPassword = await CreateHash(password);
+          new Admin({
+            name: req.body.name,
+            phone: req.body.phone,
+            email,
+            password: hashedPassword,
+            roles: req.body.roles,
+          })
+            .save()
+            .then((result) => {
+              const { _id } = result;
+              const token = jwt.sign(
+                { _id, isAdmin: true },
+                process.env.JWT_SECRET,
+                jwtConfig
+              );
+              res.send({
+                message: "Login Successful",
+                token,
+              });
+            })
+            .catch((error) => {
+              res.status(400).send({ message: "error", error });
+            });
+        } else {
+          res.status(404).send({ message: "User not found" });
+        }
+      })
+      .catch((error) => {
+        res.status(400).send({ message: "error", error });
+      });
+  }
+};
+
 const Get = (req, res) => {
   const { user } = req.auth;
   const { name, phone } = user;
   res.send({ name, phone });
+};
+
+const GetAdmin = (req, res) => {
+  const { user } = req.auth;
+  const { name, phone, email, roles } = user;
+  res.send({ name, phone, email, roles });
 };
 
 const GetOTP = (req, res) => {
@@ -84,4 +153,4 @@ const GetOTP = (req, res) => {
   }
 };
 
-module.exports = { Login, Get, GetOTP };
+module.exports = { AdminLogin, GetAdmin, Login, Get, GetOTP };
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const { CheckLogin } = require("../middlewares/auth");
 const auth = require("../controllers/auth");
 
+// Admin Auth
+router.post("/admin", auth.AdminLogin);
+router.get("/admin", CheckLogin, auth.GetAdmin);
+
+// User Auth
 router.post("/", auth.Login);
 router.get("/", CheckLogin, auth.Get);
 router.post("/otp", auth.GetOTP);
