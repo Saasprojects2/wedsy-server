e302dfa2f6dee58c838b12956ad774b11aab5b5a
hariomvc <chaudharihariomvijay@gmail.com>
2023-11-01 11:13:08 +0530

    Updated Authentication Middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -50,4 +50,39 @@ function CheckLogin(req, res, next) {
   });
 }
 
-module.exports = { CheckLogin };
+function CheckAdminLogin(req, res, next) {
+  if (!req.headers.authorization) {
+    res.status(400).send({ message: "No Auth Token" });
+    return;
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) {
+    res.status(400).send({ message: "No Auth Token" });
+    return;
+  }
+  jwt.verify(token, process.env.JWT_SECRET, function (err, result) {
+    if (err) {
+      res.status(400).send({ message: "error", error: err });
+    } else {
+      const { _id, isAdmin } = result;
+      if (_id && isAdmin) {
+        Admin.findById({ _id })
+          .then((user) => {
+            if (!user) {
+              res.status(401).send({ message: "invalid user" });
+            } else {
+              req.auth = { user_id: _id, user, roles: user.roles };
+              next();
+            }
+          })
+          .catch((error) => {
+            res.status(400).send({ message: "error", error });
+          });
+      } else {
+        res.status(400).send({ message: "unknown error" });
+      }
+    }
+  });
+}
+
+module.exports = { CheckLogin, CheckAdminLogin };
