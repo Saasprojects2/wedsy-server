72a8b55e0c5da2b2f4b4a11bdc7a3ea12fe0a20c
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2023-12-01 16:14:55 +0530

    Added Payment Model

---
+++ models/Payment.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const PaymentSchema = new mongoose.Schema(
+  {
+    user: { type: ObjectId, ref: "User", required: true },
+    event: { type: ObjectId, ref: "Event", required: true },
+    eventDay: { type: String, required: true },
+    amount: { type: Number, required: true },
+    amountPaid: { type: Number, required: true },
+    amountDue: { type: Number, required: true },
+    razporPayId: { type: String, default: "" },
+    response: { type: [Object], default: [] },
+    status: {
+      type: String,
+      required: true,
+      default: "null",
+      enum: [
+        "null",
+        "created",
+        "attempted",
+        "paid",
+        "partially_paid",
+        "expired",
+        "canceled",
+      ],
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Payment", PaymentSchema);
