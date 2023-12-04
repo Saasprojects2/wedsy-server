1b6177dffad1682e79409edff570b5657b872a22
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2023-12-05 01:06:13 +0530

    Modified API to check existing decor id

--- controllers/decor.js
+++ controllers/decor.js
@@ -58,114 +58,128 @@ const GetAll = (req, res) => {
     stageSizeHigher,
     priceLower,
     priceHigher,
+    checkId,
   } = req.query;
-  const query = {};
-  const sortQuery = {};
-  if (category) {
-    query.category = category;
-  }
-  if (search) {
-    query.$or = [
-      { name: { $regex: new RegExp(search, "i") } },
-      { description: { $regex: new RegExp(search, "i") } },
-      { tags: { $regex: new RegExp(search, "i") } },
-      { "productInfo.includes": { $regex: new RegExp(search, "i") } },
-    ];
-  }
-  if (!stageSizeLower && stageSizeHigher) {
-    query.$expr = {
-      $and: [
-        {
-          $gte: [
-            {
-              $multiply: [
-                "$productInfo.measurements.length",
-                "$productInfo.measurements.width",
-              ],
-            },
-            stageSizeLower,
-          ],
-        },
-        {
-          $lte: [
-            {
-              $multiply: [
-                "$productInfo.measurements.length",
-                "$productInfo.measurements.width",
-              ],
-            },
-            stageSizeHigher,
-          ],
-        },
-      ],
-    };
-  }
-  if (occassion) {
-    query["productVariation.occassion"] = { $in: occassion.split("|") };
-  }
-  if (color) {
-    query["productVariation.colors"] = {
-      $in: color.split("|").map((i) => i.toLowerCase()),
-    };
-  }
-  if (style && style !== "Both") {
-    query["productVariation.style"] = style;
-  }
-  if (priceLower && priceHigher) {
-    query["productInfo.variant.artificialFlowers.sellingPrice"] = {
-      $gte: priceLower,
-      $lte: priceHigher,
-    };
-    query["productInfo.variant.mixedFlowers.sellingPrice"] = {
-      $gte: priceLower,
-      $lte: priceHigher,
-    };
-    query["productInfo.variant.naturalFlowers.sellingPrice"] = {
-      $gte: priceLower,
-      $lte: priceHigher,
-    };
-    // } else if (priceLower) {
-    //   query["productInfo.sellingPrice"] = { $gte: priceLower };
-    // } else if (priceHigher) {
-    //   query["productInfo.sellingPrice"] = { $lte: priceHigher };
-  }
-  if (sort) {
-    if (sort === "Price:Low-to-High") {
-      sortQuery["productInfo.variant.artificialFlowers.sellingPrice"] = 1;
-    } else if (sort === "Price:High-to-Low") {
-      sortQuery["productInfo.variant.artificialFlowers.sellingPrice"] = -1;
-    }
-  }
-
-  Decor.countDocuments(query)
-    .then((total) => {
-      const totalPages = Math.ceil(total / limit);
-      const validPage = page % totalPages;
-      const skip =
-        validPage === 0 || validPage === null || validPage === undefined
-          ? 0
-          : (validPage - 1) * limit;
-      Decor.find(query)
-        .sort(sortQuery)
-        .skip(skip)
-        .limit(limit)
-        .exec()
-        .then((result) => {
-          res.send({ list: result, totalPages, page, limit });
-        })
-        .catch((error) => {
-          res.status(400).send({
-            message: "error",
-            error,
-          });
+  if (checkId) {
+    Decor.find({ "productInfo.id": checkId })
+      .then((result) => {
+        res.send({ id: checkId, isValid: !Boolean(result.length) });
+      })
+      .catch((error) => {
+        res.status(400).send({
+          message: "error",
+          error,
         });
-    })
-    .catch((error) => {
-      res.status(400).send({
-        message: "error",
-        error,
       });
-    });
+  } else {
+    const query = {};
+    const sortQuery = {};
+    if (category) {
+      query.category = category;
+    }
+    if (search) {
+      query.$or = [
+        { name: { $regex: new RegExp(search, "i") } },
+        { description: { $regex: new RegExp(search, "i") } },
+        { tags: { $regex: new RegExp(search, "i") } },
+        { "productInfo.includes": { $regex: new RegExp(search, "i") } },
+      ];
+    }
+    if (!stageSizeLower && stageSizeHigher) {
+      query.$expr = {
+        $and: [
+          {
+            $gte: [
+              {
+                $multiply: [
+                  "$productInfo.measurements.length",
+                  "$productInfo.measurements.width",
+                ],
+              },
+              stageSizeLower,
+            ],
+          },
+          {
+            $lte: [
+              {
+                $multiply: [
+                  "$productInfo.measurements.length",
+                  "$productInfo.measurements.width",
+                ],
+              },
+              stageSizeHigher,
+            ],
+          },
+        ],
+      };
+    }
+    if (occassion) {
+      query["productVariation.occassion"] = { $in: occassion.split("|") };
+    }
+    if (color) {
+      query["productVariation.colors"] = {
+        $in: color.split("|").map((i) => i.toLowerCase()),
+      };
+    }
+    if (style && style !== "Both") {
+      query["productVariation.style"] = style;
+    }
+    if (priceLower && priceHigher) {
+      query["productInfo.variant.artificialFlowers.sellingPrice"] = {
+        $gte: priceLower,
+        $lte: priceHigher,
+      };
+      query["productInfo.variant.mixedFlowers.sellingPrice"] = {
+        $gte: priceLower,
+        $lte: priceHigher,
+      };
+      query["productInfo.variant.naturalFlowers.sellingPrice"] = {
+        $gte: priceLower,
+        $lte: priceHigher,
+      };
+      // } else if (priceLower) {
+      //   query["productInfo.sellingPrice"] = { $gte: priceLower };
+      // } else if (priceHigher) {
+      //   query["productInfo.sellingPrice"] = { $lte: priceHigher };
+    }
+    if (sort) {
+      if (sort === "Price:Low-to-High") {
+        sortQuery["productInfo.variant.artificialFlowers.sellingPrice"] = 1;
+      } else if (sort === "Price:High-to-Low") {
+        sortQuery["productInfo.variant.artificialFlowers.sellingPrice"] = -1;
+      }
+    }
+
+    Decor.countDocuments(query)
+      .then((total) => {
+        const totalPages = Math.ceil(total / limit);
+        const validPage = page % totalPages;
+        const skip =
+          validPage === 0 || validPage === null || validPage === undefined
+            ? 0
+            : (validPage - 1) * limit;
+        Decor.find(query)
+          .sort(sortQuery)
+          .skip(skip)
+          .limit(limit)
+          .exec()
+          .then((result) => {
+            res.send({ list: result, totalPages, page, limit });
+          })
+          .catch((error) => {
+            res.status(400).send({
+              message: "error",
+              error,
+            });
+          });
+      })
+      .catch((error) => {
+        res.status(400).send({
+          message: "error",
+          error,
+        });
+      });
+  }
 };
 
 const Get = (req, res) => {
