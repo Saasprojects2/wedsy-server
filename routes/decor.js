3e6a86a993197975e7dadb3627d2029f71618f6d
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2023-12-25 13:08:34 +0530

    Modifed Decor Routes

--- routes/decor.js
+++ routes/decor.js
@@ -2,11 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const decor = require("../controllers/decor");
+const { CheckLogin, CheckAdminLogin } = require("../middlewares/auth");
 
-router.post("/", decor.CreateNew);
+router.post("/", CheckAdminLogin, decor.CreateNew);
 router.get("/", decor.GetAll);
 router.get("/:_id", decor.Get);
-router.put("/:_id", decor.Update);
-router.delete("/:_id", decor.Delete);
+router.put("/:_id", CheckAdminLogin, decor.Update);
+router.delete("/:_id", CheckAdminLogin, decor.Delete);
 
 module.exports = router;
