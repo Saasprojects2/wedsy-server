8604abe0d3b216fecb913fee986d27f65d1e9ae9
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-01-28 20:58:27 +0530

    Updated Payment Routes

--- routes/payment.js
+++ routes/payment.js
@@ -6,6 +6,7 @@ const payment = require("../controllers/payment");
 
 router.get("/", CheckLogin, payment.GetAllPayments);
 router.post("/", CheckLogin, payment.CreateEventPayment);
+router.get("/:order_id/transactions", CheckLogin, payment.GetAllTransactions);
 router.put("/:order_id", CheckLogin, payment.UpdatePayment);
 
 module.exports = router;
