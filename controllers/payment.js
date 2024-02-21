d9ed00cec58d3b6a703fae2fe69c99eb94021452
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-02-21 23:00:26 +0530

    Updated Payment Controller

--- controllers/payment.js
+++ controllers/payment.js
@@ -6,102 +6,131 @@ const {
   GetPaymentTransactions,
 } = require("../utils/payment");
 
-const CreateEventPayment = (req, res) => {
+const CreateNewPayment = (req, res) => {
   const { user_id } = req.auth;
-  const { eventId, eventDayId, paymentMethod } = req.body;
-  Event.findOne({
-    _id: eventId,
-    user: user_id,
-    eventDays: { $elemMatch: { _id: eventDayId } },
-  })
-    .then((result) => {
-      if (result) {
-        const { eventDays } = result;
-        let eventDay = eventDays.filter((item) => item._id == eventDayId)[0];
-        if (eventDay) {
-          const { finalized, approved, paymentDone } = eventDay.status;
-          if (finalized && approved && !paymentDone) {
-            const decorItems = eventDay.decorItems;
-            const packages = eventDay.packages;
-            let decorItemsAmount = decorItems.reduce(
-              (accumulator, currentObject) => {
-                return accumulator + currentObject.price;
-              },
-              0
-            );
-            let packagesAmount = packages.reduce(
-              (accumulator, currentObject) => {
-                return accumulator + currentObject.price;
-              },
-              0
-            );
-            let other_costs = eventDay.other_costs.total_other_costs || 0;
-            let amount = decorItemsAmount + packagesAmount + other_costs;
-            amount *= 100;
-            if (amount > 0) {
-              Payment.findOne({
-                user: user_id,
-                event: eventId,
-                eventDay: eventDayId,
-              })
-                .then((paymentFound) => {
-                  if (paymentFound) {
-                    res.status(200).send({
-                      message: "success",
-                      _id: paymentFound._id,
-                      order_id: paymentFound.razporPayId,
-                      amount: paymentFound.amount,
-                    });
-                  } else {
-                    new Payment({
-                      user: user_id,
-                      event: eventId,
-                      eventDay: eventDayId,
-                      amount,
-                      amountPaid: 0,
-                      amountDue: amount,
-                      paymentMethod: paymentMethod || "default",
-                    })
-                      .save()
-                      .then((result) => {
-                        CreatePayment({ _id: result._id })
-                          .then((order) => {
-                            res.status(200).send({
-                              message: "success",
-                              _id: result._id,
-                              order_id: order.id,
-                              amount,
-                            });
-                          })
-                          .catch((error) => {
-                            res.status(400).send({ message: "error", error });
-                          });
-                      })
-                      .catch((error) => {
-                        res.status(400).send({ message: "error", error });
-                      });
-                  }
-                })
-                .catch((error) => {
-                  res.status(400).send({ message: "error", error });
-                });
-            } else {
-              res.status(400).send({ message: "No items to pay" });
-            }
-          } else {
-            res.status(400).send({ message: "Event not ready for payment." });
-          }
-        } else {
-          res.status(404).send({ message: "Event not found" });
-        }
-        // res.status(200).send({ message: "success" });
-      } else {
-        res.status(404).send({ message: "Event not found" });
-      }
+  const { event, paymentFor, paymentMethod, amount } = req.body;
+  if (paymentFor === "event" && event && paymentMethod === "razporpay") {
+    new Payment({
+      user: user_id,
+      event: event,
+      paymentFor,
+      paymentMethod,
+      amount: amount * 100,
+      amountPaid: 0,
+      amountDue: amount * 100,
     })
-    .catch((error) => {
-      res.status(400).send({ message: "error", error });
-    });
+      .save()
+      .then((result) => {
+        CreatePayment({ _id: result._id })
+          .then((order) => {
+            res.status(200).send({
+              message: "success",
+              _id: result._id,
+              order_id: order.id,
+              amount: amount * 100,
+            });
+          })
+          .catch((error) => {
+            res.status(400).send({ message: "error", error });
+          });
+      })
+      .catch((error) => {
+        res.status(400).send({ message: "error", error });
+      });
+  }
+  // Event.findOne({
+  //   _id: eventId,
+  //   user: user_id,
+  //   eventDays: { $elemMatch: { _id: eventDayId } },
+  // })
+  //   .then((result) => {
+  //     if (result) {
+  //       const { eventDays } = result;
+  //       let eventDay = eventDays.filter((item) => item._id == eventDayId)[0];
+  //       if (eventDay) {
+  //         const { finalized, approved, paymentDone } = eventDay.status;
+  //         if (finalized && approved && !paymentDone) {
+  //           const decorItems = eventDay.decorItems;
+  //           const packages = eventDay.packages;
+  //           let decorItemsAmount = decorItems.reduce(
+  //             (accumulator, currentObject) => {
+  //               return accumulator + currentObject.price;
+  //             },
+  //             0
+  //           );
+  //           let packagesAmount = packages.reduce(
+  //             (accumulator, currentObject) => {
+  //               return accumulator + currentObject.price;
+  //             },
+  //             0
+  //           );
+  //           let other_costs = eventDay.other_costs.total_other_costs || 0;
+  //           let amount = decorItemsAmount + packagesAmount + other_costs;
+  //           amount *= 100;
+  //           if (amount > 0) {
+  //             Payment.findOne({
+  //               user: user_id,
+  //               event: eventId,
+  //               eventDay: eventDayId,
+  //             })
+  //               .then((paymentFound) => {
+  //                 if (paymentFound) {
+  //                   res.status(200).send({
+  //                     message: "success",
+  //                     _id: paymentFound._id,
+  //                     order_id: paymentFound.razporPayId,
+  //                     amount: paymentFound.amount,
+  //                   });
+  //                 } else {
+  //                   new Payment({
+  //                     user: user_id,
+  //                     event: eventId,
+  //                     eventDay: eventDayId,
+  //                     amount,
+  //                     amountPaid: 0,
+  //                     amountDue: amount,
+  //                     paymentMethod: paymentMethod || "default",
+  //                   })
+  //                     .save()
+  //                     .then((result) => {
+  //                       CreatePayment({ _id: result._id })
+  //                         .then((order) => {
+  //                           res.status(200).send({
+  //                             message: "success",
+  //                             _id: result._id,
+  //                             order_id: order.id,
+  //                             amount,
+  //                           });
+  //                         })
+  //                         .catch((error) => {
+  //                           res.status(400).send({ message: "error", error });
+  //                         });
+  //                     })
+  //                     .catch((error) => {
+  //                       res.status(400).send({ message: "error", error });
+  //                     });
+  //                 }
+  //               })
+  //               .catch((error) => {
+  //                 res.status(400).send({ message: "error", error });
+  //               });
+  //           } else {
+  //             res.status(400).send({ message: "No items to pay" });
+  //           }
+  //         } else {
+  //           res.status(400).send({ message: "Event not ready for payment." });
+  //         }
+  //       } else {
+  //         res.status(404).send({ message: "Event not found" });
+  //       }
+  //       // res.status(200).send({ message: "success" });
+  //     } else {
+  //       res.status(404).send({ message: "Event not found" });
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     res.status(400).send({ message: "error", error });
+  //   });
 };
 
 const UpdatePayment = (req, res) => {
@@ -113,21 +142,22 @@ const UpdatePayment = (req, res) => {
       if (order.status === "paid") {
         Payment.findOne({ user: user_id, razporPayId: order_id })
           .then((payment) => {
-            const { event, eventDay } = payment;
-            Event.findOneAndUpdate(
-              {
-                _id: event,
-                user: user_id,
-                eventDays: { $elemMatch: { _id: eventDay } },
-              },
-              { $set: { "eventDays.$.status.paymentDone": true } }
-            )
-              .then((result) => {
-                res.status(200).send({ message: "success" });
-              })
-              .catch((error) => {
-                res.status(400).send({ message: "error", error });
-              });
+            res.status(200).send({ message: "success" });
+            // const { event, eventDay } = payment;
+            // Event.findOneAndUpdate(
+            //   {
+            //     _id: event,
+            //     user: user_id,
+            //     eventDays: { $elemMatch: { _id: eventDay } },
+            //   },
+            //   { $set: { "eventDays.$.status.paymentDone": true } }
+            // )
+            //   .then((result) => {
+            //     res.status(200).send({ message: "success" });
+            //   })
+            //   .catch((error) => {
+            //     res.status(400).send({ message: "error", error });
+            //   });
           })
           .catch((error) => {
             res.status(400).send({ message: "error", error });
@@ -236,7 +266,7 @@ const GetAllTransactions = (req, res) => {
 };
 
 module.exports = {
-  CreateEventPayment,
+  CreateNewPayment,
   UpdatePayment,
   GetAllPayments,
   GetAllTransactions,
