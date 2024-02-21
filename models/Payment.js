de320f7f6d107c135643fc74195882625648d67a
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-02-21 22:59:30 +0530

    Updated Payment Model

--- models/Payment.js
+++ models/Payment.js
@@ -3,12 +3,21 @@ const ObjectId = mongoose.Schema.Types.ObjectId;
 
 const PaymentSchema = new mongoose.Schema(
   {
+    // User (the person doing the payment, or for whom the payment is done.)
     user: { type: ObjectId, ref: "User", required: true },
-    event: { type: ObjectId, ref: "Event", required: true },
-    eventDay: { type: String, required: true },
-    amount: { type: Number, required: true },
-    amountPaid: { type: Number, required: true },
-    amountDue: { type: Number, required: true },
+    // Amount (Total, Paid, Due)
+    amount: { type: Number, required: true, default: 0 },
+    amountPaid: { type: Number, required: true, default: 0 },
+    amountDue: { type: Number, required: true, default: 0 },
+    // Payment For? Event or?
+    paymentFor: {
+      type: String,
+      default: "default",
+      enum: ["default", "event"],
+      required: true,
+    },
+    event: { type: ObjectId, ref: "Event" },
+    // Payent Method (Cash [Added by Admin], Razorpay)
     paymentMethod: {
       type: String,
       default: "default",
@@ -17,6 +26,7 @@ const PaymentSchema = new mongoose.Schema(
     },
     razporPayId: { type: String, default: "" },
     response: { type: [Object], default: [] },
+    // Payment Status: null, created, attempted, paid, partially paid, expired, cancelled
     status: {
       type: String,
       required: true,
