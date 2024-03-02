ec6919f5b71b9cbc3879f80e68f2a31e1997989c
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-03-02 22:33:15 +0530

    Updated Auth Middleware, added middleware to check token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -2,6 +2,105 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 const Admin = require("../models/Admin");
 
+function CheckToken(req, res, next) {
+  if (!req.headers.authorization) {
+    req.auth = {
+      user_id: "",
+      user: {},
+      isAdmin: false,
+    };
+    next();
+    return;
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) {
+    req.auth = {
+      user_id: "",
+      user: {},
+      isAdmin: false,
+    };
+    next();
+    return;
+  }
+  jwt.verify(token, process.env.JWT_SECRET, function (err, result) {
+    if (err) {
+      req.auth = {
+        user_id: "",
+        user: {},
+        isAdmin: false,
+      };
+      next();
+      return;
+    } else {
+      const { _id, isAdmin } = result;
+      if (_id && isAdmin) {
+        Admin.findById({ _id })
+          .then((user) => {
+            if (!user) {
+              req.auth = {
+                user_id: "",
+                user: {},
+                isAdmin: false,
+              };
+              next();
+              return;
+            } else {
+              req.auth = {
+                user_id: _id,
+                user,
+                roles: user.roles,
+                isAdmin: true,
+              };
+              next();
+            }
+          })
+          .catch((error) => {
+            req.auth = {
+              user_id: "",
+              user: {},
+              isAdmin: false,
+            };
+            next();
+            return;
+          });
+      } else if (_id) {
+        User.findById({ _id })
+          .then((user) => {
+            if (!user) {
+              req.auth = {
+                user_id: "",
+                user: {},
+                isAdmin: false,
+              };
+              next();
+              return;
+            } else {
+              req.auth = { user_id: _id, user, isAdmin: false };
+              next();
+            }
+          })
+          .catch((error) => {
+            req.auth = {
+              user_id: "",
+              user: {},
+              isAdmin: false,
+            };
+            next();
+            return;
+          });
+      } else {
+        req.auth = {
+          user_id: "",
+          user: {},
+          isAdmin: false,
+        };
+        next();
+        return;
+      }
+    }
+  });
+}
+
 function CheckLogin(req, res, next) {
   if (!req.headers.authorization) {
     res.status(400).send({ message: "No Auth Token" });
@@ -95,4 +194,4 @@ function CheckAdminLogin(req, res, next) {
   });
 }
 
-module.exports = { CheckLogin, CheckAdminLogin };
+module.exports = { CheckLogin, CheckAdminLogin, CheckToken };
