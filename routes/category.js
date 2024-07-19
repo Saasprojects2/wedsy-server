fd872528f9a12e1037dfe50a94eee3f72b7efdc6
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-07-19 09:31:36 +0530

    Updated Category Routes

--- routes/category.js
+++ routes/category.js
@@ -5,7 +5,7 @@ const category = require("../controllers/category");
 const { CheckLogin, CheckAdminLogin } = require("../middlewares/auth");
 
 router.post("/", CheckAdminLogin, category.CreateNew);
-router.get("/", CheckAdminLogin, category.GetAll);
+router.get("/", category.GetAll);
 router.get("/:_id", CheckAdminLogin, category.Get);
 router.put("/:_id", CheckAdminLogin, category.Update);
 router.delete("/:_id", CheckAdminLogin, category.Delete);
