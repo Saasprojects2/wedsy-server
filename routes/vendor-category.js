0dd9e801b29344dc558fc01c5eb3bd7a0105a53a
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-09-08 09:21:47 +0530

    Updated Vendor Categroy Routes

--- routes/vendor-category.js
+++ routes/vendor-category.js
@@ -5,7 +5,7 @@ const vendorCategory = require("../controllers/vendor-category");
 const { CheckLogin, CheckAdminLogin } = require("../middlewares/auth");
 
 router.post("/", CheckAdminLogin, vendorCategory.CreateNew);
-router.get("/", CheckAdminLogin, vendorCategory.GetAll);
+router.get("/", vendorCategory.GetAll);
 router.get("/:_id", CheckAdminLogin, vendorCategory.Get);
 router.put("/:_id", CheckAdminLogin, vendorCategory.Update);
 router.delete("/:_id", CheckAdminLogin, vendorCategory.Delete);
