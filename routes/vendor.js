2a910e2dddab8198a23f501a65a76b07d6dbee54
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2024-09-08 09:20:58 +0530

    Updated Vendor Routes

--- routes/vendor.js
+++ routes/vendor.js
@@ -2,11 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 const vendor = require("../controllers/vendor");
-const { CheckLogin, CheckAdminLogin } = require("../middlewares/auth");
+const {
+  CheckLogin,
+  CheckAdminLogin,
+  CheckVendorLogin,
+} = require("../middlewares/auth");
 
 router.post("/", vendor.CreateNew);
 router.get("/", CheckAdminLogin, vendor.GetAll);
 router.get("/:_id", CheckAdminLogin, vendor.Get);
+router.post("/:_id/notes", vendor.AddNotes);
+router.put("/", CheckVendorLogin, vendor.Update);
 router.put("/:_id", CheckAdminLogin, vendor.Update);
 router.delete("/:_id", CheckAdminLogin, vendor.Delete);
 router.delete("/", CheckAdminLogin, vendor.DeleteVendors);
