6e942411038e2e8017621e0cefc2d93d10331e75
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2025-01-18 11:10:26 +0530

    Added Chat Content Model

---
+++ models/ChatContent.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const ChatContentSchema = new mongoose.Schema(
+  {
+    chat: { type: ObjectId, ref: "Chat", required: true },
+    contentType: { type: String, default: "" },
+    // BiddingBid/BiddingOffer/Text/PersonalPackageAccepted
+    content: { type: String, default: "" },
+    other: { type: Object, default: {} },
+    sender: {
+      id: { type: ObjectId, required: false, default: null },
+      role: {
+        type: String,
+        enum: ["", "user", "vendor", "admin"],
+        default: "",
+      },
+    },
+    status: {
+      viewedByUser: { type: Boolean, default: false },
+      viewedByVendor: { type: Boolean, default: false },
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("ChatContent", ChatContentSchema);
