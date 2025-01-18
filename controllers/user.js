0ff2d3725556ce8f030c98272ee44760b7c231e8
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2025-01-18 10:55:11 +0530

    Updated User Controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const UserSavedAddress = require("../models/UserSavedAddress");
 
 const GetUser = (req, res) => {
   const { user_id } = req.auth;
@@ -97,6 +98,25 @@ const AddToWishList = (req, res) => {
           error,
         });
       });
+  } else if (wishlist === "vendor") {
+    User.findByIdAndUpdate(
+      { _id: user_id },
+      { $addToSet: { "wishlist.vendor": _id } }
+    )
+      .exec()
+      .then((result) => {
+        if (result) {
+          res.send({ message: "success" });
+        } else {
+          res.status(400).send();
+        }
+      })
+      .catch((error) => {
+        res.status(400).send({
+          message: "error",
+          error,
+        });
+      });
   }
 };
 
@@ -129,6 +149,25 @@ const RemoveFromWishList = (req, res) => {
     User.findByIdAndUpdate(
       { _id: user_id },
       { $pull: { "wishlist.decorPackage": _id } }
+    )
+      .exec()
+      .then((result) => {
+        if (result) {
+          res.send({ message: "success" });
+        } else {
+          res.status(400).send();
+        }
+      })
+      .catch((error) => {
+        res.status(400).send({
+          message: "error",
+          error,
+        });
+      });
+  } else if (wishlist === "vendor") {
+    User.findByIdAndUpdate(
+      { _id: user_id },
+      { $pull: { "wishlist.vendor": _id } }
     )
       .exec()
       .then((result) => {
@@ -183,6 +222,19 @@ const GetWishList = (req, res) => {
           error,
         });
       });
+  } else if (wishlist === "vendor") {
+    User.findById({ _id: user_id }, "wishlist")
+      .populate("wishlist.vendor")
+      .exec()
+      .then((result) => {
+        res.send(result.wishlist.vendor);
+      })
+      .catch((error) => {
+        res.status(400).send({
+          message: "error",
+          error,
+        });
+      });
   }
 };
 
@@ -208,6 +260,12 @@ const IsAddedToWishlist = (req, res) => {
           } else {
             res.send({ message: "success", wishlist: false });
           }
+        } else if (product === "vendor") {
+          if (wishlist.vendor.includes(_id)) {
+            res.send({ message: "success", wishlist: true });
+          } else {
+            res.send({ message: "success", wishlist: false });
+          }
         } else {
           res.send({ message: "success", wishlist: false });
         }
@@ -221,6 +279,76 @@ const IsAddedToWishlist = (req, res) => {
   }
 };
 
+const GetUserSavedAddress = (req, res) => {
+  const { user_id, isAdmin, isVendor } = req.auth;
+  if (!isAdmin && !isVendor) {
+    UserSavedAddress.find({ user: user_id })
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((error) => {
+        res.status(400).send({
+          message: "error",
+          error,
+        });
+      });
+  } else {
+    res.status(400).send({
+      message: "error",
+      error,
+    });
+  }
+};
+
+const AddSavedAddress = (req, res) => {
+  const { user_id, isAdmin, isVendor } = req.auth;
+  if (!isAdmin && !isVendor) {
+    const {
+      place_id,
+      formatted_address,
+      address_components,
+      house_no,
+      city,
+      postal_code,
+      locality,
+      state,
+      country,
+      geometry,
+      address_type,
+    } = req.body;
+    if (!place_id) {
+      res.status(400).send({ message: "Incomplete Data" });
+    } else {
+      new UserSavedAddress({
+        user: user_id,
+        place_id,
+        formatted_address,
+        address_components,
+        house_no,
+        city,
+        postal_code,
+        locality,
+        state,
+        country,
+        geometry,
+        address_type,
+      })
+        .save()
+        .then((result) => {
+          res.status(201).send({ message: "success", id: result._id });
+        })
+        .catch((error) => {
+          res.status(400).send({ message: "error", error });
+        });
+    }
+  } else {
+    res.status(400).send({
+      message: "error",
+      error,
+    });
+  }
+};
+
 module.exports = {
   GetUser,
   UpdateUser,
@@ -229,4 +357,6 @@ module.exports = {
   GetWishListAll,
   RemoveFromWishList,
   IsAddedToWishlist,
+  GetUserSavedAddress,
+  AddSavedAddress,
 };
