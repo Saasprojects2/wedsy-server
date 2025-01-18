bfb3129423468aae3ea6754499b8a10fc0b6529e
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2025-01-18 11:15:30 +0530

    Added Vendor Personal Package Booking Model

---
+++ models/VendorPersonalPackage.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const VendorPersonalPackageSchema = new mongoose.Schema(
+  {
+    vendor: { type: ObjectId, required: true },
+    name: { type: String, required: true },
+    services: { type: [String], default: []},
+    price: { type: Number, required: true, default: 0 },
+    amountToVendor: { type: Number, required: true, default: 0 },
+    amountToWedsy: { type: Number, required: true, default: 0 },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model(
+  "VendorPersonalPackage",
+  VendorPersonalPackageSchema
+);
