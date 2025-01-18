54b21bc6cb82788cd42a9f71a04b8181b056ec6a
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2025-01-18 11:04:16 +0530

    Updated Vendor Add Ons Router

--- routes/vendor-add-ons.js
+++ routes/vendor-add-ons.js
@@ -5,7 +5,7 @@ const vendorAddOns = require("../controllers/vendor-add-ons");
 const { CheckLogin, CheckAdminLogin } = require("../middlewares/auth");
 
 router.post("/", CheckAdminLogin, vendorAddOns.CreateNew);
-router.get("/", CheckAdminLogin, vendorAddOns.GetAll);
+router.get("/", vendorAddOns.GetAll);
 router.get("/:_id", CheckAdminLogin, vendorAddOns.Get);
 router.put("/:_id", CheckAdminLogin, vendorAddOns.Update);
 router.post("/:_id/makeup-style", CheckAdminLogin, vendorAddOns.AddMakeupStyle);
