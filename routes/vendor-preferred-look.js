e19ec48976a863e14a1cf1ff80c982b6bc71f4f7
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2025-01-18 11:05:20 +0530

    Updated Vendor Preferred Look Router

--- routes/vendor-preferred-look.js
+++ routes/vendor-preferred-look.js
@@ -5,7 +5,7 @@ const vendorPreferredLook = require("../controllers/vendor-preferred-look");
 const { CheckLogin, CheckAdminLogin } = require("../middlewares/auth");
 
 router.post("/", CheckAdminLogin, vendorPreferredLook.CreateNew);
-router.get("/", CheckAdminLogin, vendorPreferredLook.GetAll);
+router.get("/", vendorPreferredLook.GetAll);
 router.get("/:_id", CheckAdminLogin, vendorPreferredLook.Get);
 router.put("/:_id", CheckAdminLogin, vendorPreferredLook.Update);
 router.delete("/:_id", CheckAdminLogin, vendorPreferredLook.Delete);
