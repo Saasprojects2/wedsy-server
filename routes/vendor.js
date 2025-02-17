3a145e195836f2d985717347b7920341516fbfa6
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2025-02-17 21:35:37 +0530

    Updated Vendor Router:

--- routes/vendor.js
+++ routes/vendor.js
@@ -12,6 +12,7 @@ const {
 router.post("/", vendor.CreateNew);
 router.get("/", CheckToken, vendor.GetAll);
 router.get("/:_id", CheckToken, vendor.Get);
+router.get("/:_id/last-active", CheckAdminLogin, vendor.GetVendorLastActive);
 router.post("/:_id/notes", CheckAdminLogin, vendor.AddNotes);
 router.put("/", CheckVendorLogin, vendor.Update);
 router.put("/:_id", CheckAdminLogin, vendor.Update);
