67681b394eaf1203f807bb19c905649ba4d2741f
Hariom Chaudhari <chaudharihariomvijay@gmail.com>
2025-02-24 22:00:25 +0530

    Updated Vendor Personal Package Model

--- models/VendorPersonalPackage.js
+++ models/VendorPersonalPackage.js
@@ -5,10 +5,11 @@ const VendorPersonalPackageSchema = new mongoose.Schema(
   {
     vendor: { type: ObjectId, required: true },
     name: { type: String, required: true },
-    services: { type: [String], default: []},
+    services: { type: [String], default: [] },
     price: { type: Number, required: true, default: 0 },
     amountToVendor: { type: Number, required: true, default: 0 },
     amountToWedsy: { type: Number, required: true, default: 0 },
+    active: { type: Boolean, required: true, default: true },
   },
   { timestamps: true }
 );
